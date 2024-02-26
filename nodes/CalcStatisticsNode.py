cfd4f98a5df666b0575e7117943b6805239c0bbc
Колесников Дмитрий Андреевич <kolesnikovda@digtp.com>
2024-02-26 14:09:22 +0300

    верный расчет статистики по скользящему окну

--- nodes/CalcStatisticsNode.py
+++ nodes/CalcStatisticsNode.py
@@ -2,7 +2,6 @@ from collections import deque
 import numpy as np
 
 from elements.FrameElement import FrameElement
-from elements.TrackElement import TrackElement
 from utils_local.utils import profile_time
 
 class CalcStatisticsNode:
@@ -11,7 +10,8 @@ class CalcStatisticsNode:
     def __init__(self, config: dict) -> None:
         config_general = config["general"]
 
-        self.min_time_life_track = config_general["min_time_life_track"]
+        self.time_buffer_analytics = config_general["buffer_analytics"]  # размер времени буфера в минутах
+        self.min_time_life_track = config_general["min_time_life_track"]  # минимальное время жизни трека в сек
         self.count_cars_buffer_frames = config_general["count_cars_buffer_frames"]
         self.cars_buffer = deque(maxlen=self.count_cars_buffer_frames)  # создали буфер значений
           
@@ -22,9 +22,29 @@ class CalcStatisticsNode:
 
         info_dictionary = {}
         info_dictionary['cars_amount'] = round(np.mean(self.cars_buffer))
+        roads_activity = {1:0, 2:0, 3:0, 4:0, 5:0}  # всего 5 дорог (занулим стартовое значение)
+
+        # Посчитаем чило машин которые довно живут и имеют значения дороги приезда
+        for _, track_element in buffer_tracks.items():
+            if (
+                track_element.timestamp_last - track_element.timestamp_init_road > self.min_time_life_track and
+                track_element.start_road is not None
+            ):
+                key = track_element.start_road
+                roads_activity[key] += 1
+
+        # Переведем значения в размерность машин/мин согласно известному размеру буфера
+        for key in roads_activity:
+            roads_activity[key] /= self.time_buffer_analytics
+
+        info_dictionary['roads_activity'] = roads_activity
 
         # Запись результатов обработки:
         frame_element.info = info_dictionary
+
+        # для дебага оставил:
+        #if frame_element.frame_num % 100 == 0:
+        #   print(info_dictionary)
 
         return frame_element
 
