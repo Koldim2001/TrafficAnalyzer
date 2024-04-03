1bd5f9c4b730e58671ff7b06263b2fd29bcfa0b9
Колесников Дмитрий Андреевич <kolesnikovda@digtp.com>
2024-04-03 16:55:21 +0300

    fix requirements and bat

--- run_services.bat
+++ run_services.bat
@@ -1,7 +1,2 @@
-REM Создаем необходимые папки
-mkdir .\services\pg_data_wh
-mkdir .\services\pg_grafana
-mkdir .\services\grafana
-
 REM Запускаем docker-compose
 docker-compose -p traffic_analyzer up -d
